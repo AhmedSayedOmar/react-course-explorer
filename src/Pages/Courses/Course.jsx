f35ff0763907734f5f7ab0ef874f71447b08a2ec
AhmedSayedOmar <ahmedsayed00907@gmail.com>
2026-07-08 15:12:56 +0300

    fix the image in the course carts

--- src/Pages/Courses/Course.jsx
+++ src/Pages/Courses/Course.jsx
@@ -44,7 +44,7 @@ export function Course({ course ,loadCart}) {
 
                     <div className="product-image-container">
                         <img className="product-image"
-                            src={course.image} />
+                            src={`${import.meta.env.VITE_API_URL}/${course.image}`} />
                     </div>
 
                     <div className="product-name limit-text-to-2-lines">
